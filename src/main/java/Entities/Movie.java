9b1d9f6c80a35c382193110a1e8f1278060977c8
mostafa-al-hassan <mostafadaud014014@gmail.com>
2025-11-25 20:21:51 +0200

    added year field for Movie Class

--- src/main/java/Entities/Movie.java
+++ src/main/java/Entities/Movie.java
@@ -6,12 +6,13 @@ import java.util.Comparator;
 public class Movie implements Comparable{
     private String name;
     private String id;
+    private int year;
     private ArrayList<String> genres;
 
-    public Movie(String name, String id){
+    public Movie(String name,int year, String id){
         setName(name);
         setId(id);
-
+        setYear(year);
         this.genres = new ArrayList<String>();
     }
 
@@ -24,6 +25,18 @@ public class Movie implements Comparable{
             throw new IllegalArgumentException("Movie name can't be null or empty");
 
         this.name = name;
+    }
+
+    public void setYear(int year) {
+        final int minYear = 1888;
+        final int currentYear = java.time.Year.now().getValue();
+
+        // Check if the year is outside the acceptable historical and future range
+        if (year < minYear || year > currentYear) {
+            throw new IllegalArgumentException(
+                    "Invalid year: " + year + ". Year must be between " + minYear + " and " + currentYear + ".");
+        }
+        this.year = year;
     }
 
     public void setId(String id) {
@@ -41,8 +54,16 @@ public class Movie implements Comparable{
         return id;
     }
 
+    public int getYear() {
+        return year;
+    }
+
     @Override
     public int compareTo(Object movie) {
         return this.id.compareTo(((Movie)movie).id);
     }
+
+
+
+
 }
