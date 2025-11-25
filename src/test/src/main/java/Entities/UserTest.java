9ccb69e69404f2c2437b293d935204cdebe2fa9c
mostafa-al-hassan <mostafadaud014014@gmail.com>
2025-11-25 23:31:22 +0200

    added unit test cases for User Class

---
+++ src/test/src/main/java/Entities/UserTest.java
@@ -0,0 +1,90 @@
+package src.main.java.Entities;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class UserTest {
+    private  User user;
+    Set<String> existingIds = new HashSet<>();
+
+    @BeforeEach
+    public void setUp(){
+        // Declare this as well if needed in other tests
+
+        user = new User("john", "12345678a", existingIds);
+    }
+
+    @Test
+    public void user_WithValidDetails(){
+        assertNotNull(user);
+    }
+
+    @Test
+    public void user_withInvalidName(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setName(null);
+        });;
+    }
+
+    @Test
+    public void user_withNameStartsWithSpace(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setName(" john");
+        });;
+    }
+
+
+    @Test
+    public void setName_NameWithDigits_ThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setName("John123");
+        });
+    }
+
+    @Test
+    void setName_NameWithHyphen_ThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setName("John-Doe");
+        });
+    }
+
+    @Test
+    void setId_NullId_ThrowsException() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setId(null,existingIds);
+        });
+    }
+
+    @Test
+    void setId_IdTooLong_ThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+             user.setId("1234567890", existingIds);
+        });
+    }
+
+    @Test
+    void setId_IdNotStartingWithDigit_ThrowsException() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            user.setId("J23456789", existingIds);
+        });
+        assertTrue(thrown.getMessage().contains("id must start with numbers"));
+    }
+
+    @Test
+    void setId_IdAlreadyExists_ThrowsException() {
+        // existingIds contains EXISTING_ID from setup
+        existingIds.add("12345678a");
+        assertThrows(IllegalArgumentException.class, () -> {
+            User user2 = new User("name","12345678a",existingIds);
+        });
+    }
+
+}
