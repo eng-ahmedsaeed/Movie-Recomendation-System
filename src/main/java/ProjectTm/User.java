22b33953d38631c88c6f8c02300d276c02b77f06
Seif Mustafa <saifmostafa1854@gmail.com>
2025-12-17 21:55:16 +0200

    Added Path Coverage tests for movieRecommend

--- src/main/java/ProjectTm/User.java
+++ src/main/java/ProjectTm/User.java
@@ -13,56 +13,69 @@ public class User {
     public User(String id, String name){
         this.name = name;
         this.id = id;
-        this.searchedMovie = new ArrayList<>();
-        this.recommendedMovies = new ArrayList<>();
+        this.searchedMovie = new ArrayList<Movie>();
+        this.recommendedMovies = new ArrayList<Movie>();
     }
 
-    public User(String name, String id, Set<String> existingIds){
-        setName(name);
-        setId(id, existingIds);
-        this.searchedMovie = new ArrayList<>();
-        this.recommendedMovies = new ArrayList<>();
-        existingIds.add(id);
+
+    public void printUser(){
+        System.out.println(this.name + "," + this.id);
+        for( Movie movie : recommendedMovies){
+            System.out.println(movie.getName() +","+ movie.getId());
+        }
     }
 
-    public void setId(String id, Set<String> existingIds) {
-        if (id == null || id.length() != 9)
-            throw new IllegalArgumentException("User Id length error");
-        if (existingIds.contains(id))
-            throw new IllegalArgumentException("ERROR: User Id " + id + " is wrong");
-        this.id = id;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() { return name; }
-    public String getId() { return id; }
-
-    public ArrayList<Movie> getSearchedMovie() { return this.searchedMovie; }
-    public ArrayList<Movie> getRecommendedMovies() { return this.recommendedMovies; }
-
-    public void setRecommendedMovies(ArrayList<Movie> recommendedMovies) {
-        this.recommendedMovies = recommendedMovies;
-    }
-    public void setSearchedMovie(Movie movie) { this.searchedMovie.add(movie); }
-    public void setSearchedMovieInst(ArrayList<Movie> movie) { this.searchedMovie=movie; }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof User)) return false;
         User user = (User) o;
-        return id.equals(user.id);
+        return id.equals(user.id) &&
+                name.equals(user.name) &&
+                searchedMovie.equals(user.searchedMovie)&&
+                recommendedMovies.equals(user.recommendedMovies);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, name, searchedMovie, recommendedMovies);
     }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList<Movie> getSearchedMovie() {
+        return this.searchedMovie;
+    }
+    public ArrayList<Movie> getRecommendedMovies() {
+        return this.recommendedMovies;
+    }
+
+    public void setRecommendedMovies(ArrayList<Movie> recommendedMovies) {
+        this.recommendedMovies = recommendedMovies;
+    }
+    public void setSearchedMovie(Movie movie) { this.searchedMovie.add(movie); }
+
+    public void setSearchedMovieInst(ArrayList<Movie> movie) { this.searchedMovie=movie;}
 }
+
+
+
+
+
+
+
